8ef96603fb283e3ae3311d9f358560c2642327db
Lou Alicegary <lou@ginkgotree.com>
2014-11-28 13:37:43 -0500

    Test parsing of URL parameters

--- web/index.php
+++ web/index.php
@@ -7,14 +7,31 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+
+    $app['monolog']->addDebug('logging output.');
+
+    $email = "";
+    $campaign = "";
+
+    $return_string = "";
+
+    if (isset($_GET['utm_content'])) {
+        $email = $_GET['utm_content'];
+    }
+    if (isset($_GET['utm_source'])) {
+        $campaign = $_GET['utm_source']; 
+    }
+
+    $return_string .= "Email: " . $email . "\nCampaign: " . $campaign; 
+
+    return $return_string;
+
 });
 
 $app->run();
