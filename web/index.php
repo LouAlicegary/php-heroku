b57631a3285118a11cdf79f4e23ace5323251a50
Lou Alicegary <lou@ginkgotree.com>
2014-11-28 14:21:58 -0500

    Testing RelateIQ integration

--- web/index.php
+++ web/index.php
@@ -28,7 +28,25 @@ $app->get('/', function() use($app) {
         $campaign = $_GET['utm_source']; 
     }
 
-    $return_string .= "Email: " . $email . "\nCampaign: " . $campaign; 
+
+    $RELATE_IQ_API_KEY = "54344ba3e4b00c4eb44c60dc"
+    $RELATE_IQ_API_SECRET = "XXv17NeOIBhFFBbOAjDC94MdH7A"
+    $url = "https://api.relateiq.com/v2/contacts?properties.email=lou@ginkgotree.com";
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, $RELATE_IQ_API_KEY . ":" . $RELATE_IQ_API_SECRET);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    $return_string .= "VAR DUMP: " . var_dump(json_decode($result, true));
+
+    $return_string .= "<br>Email: " . $email . "<br>Campaign: " . $campaign; 
 
     return $return_string;
 
